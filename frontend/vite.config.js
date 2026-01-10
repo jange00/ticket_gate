a37eb9ccec111d5d272a64cefd6c40bb3c456633
Sushant Mahato <sushantmahato@Sushants-MacBook-Air-2.local>
2026-01-10 10:12:45 +0545

    vite config

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+    force: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
+})
